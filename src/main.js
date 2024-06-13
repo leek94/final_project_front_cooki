8b4045d828cef3f1f0505af999312586ae71b838
shyeseon <gPtjs1222@gmail.com>
2024-06-13 16:39:33 +0900

    feat:add img

--- src/main.js
+++ src/main.js
@@ -6,5 +6,9 @@ import store from './store'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+//브라우저 재실행시 인증 정보를 가져오기
+store.dispatch("loadAuth");
+
+
 
 createApp(App).use(store).use(router).mount('#app')

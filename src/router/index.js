df938854a372e2b0193786e9451c84f5c370b1cf
shyeseon <gPtjs1222@gmail.com>
2024-06-13 16:52:15 +0900

    feat:change recipe

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import Recipe from './Recipe';
+import Recipe from './Recipe'
 import TotalSearch from './TotalSearch';
 import Class from './Class';
 import Member from './Member';

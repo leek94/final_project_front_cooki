6cb61aeeb1f439dc4109e10fe38f24cc4f21e26b
leek94 <khwan929@naver.com>
2024-06-16 13:53:02 +0900

    feat: totalSearch add

--- src/views/TotalSearchView/index.vue
+++ src/views/TotalSearchView/index.vue
@@ -2,7 +2,10 @@
     <div class="card">
         <div class="card-header">totalSearch</div>
         <div class="card-body">
-            
+            <RouterLink class="me-2" to="/TotalSearchView/Total">전체</RouterLink>
+            <RouterLink class="me-2" to="/TotalSearchView/ClassCompact">클래스</RouterLink>
+            <RouterLink class="me-2" to="/TotalSearchView/RecipeCompact">레시피</RouterLink>
+            <RouterView></RouterView>
         </div>
     </div>
 </template>

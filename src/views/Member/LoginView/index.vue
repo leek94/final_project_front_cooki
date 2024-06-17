a70417bdd1b73bc7f4a36cd537135908182c0d0b
BHM <kakao940720@gmail.com>
2024-06-17 21:11:14 +0900

    Design: Join / Login / MyProfile page ui update

--- src/views/Member/LoginView/index.vue
+++ src/views/Member/LoginView/index.vue
@@ -2,11 +2,11 @@
     <form>
 
         <div class="d-flex row" style="justify-content: center;">
-                <div class="row mx-5 p-5 " style="width: 700px; justify-content: center;">
+                <div class="row mx-5 p-5 " style="width: 500px; justify-content: center;">
                     <h3 class="mb-5" style="font-weight: bold; text-align: center">로그인</h3>
-                    <input class="p-2 mb-3" placeholder="이메일 형식의 아이디"/>
-                    <input class="p-2 mb-4" placeholder="비밀번호"/>
-                    <button class="p-2 w-100 bg-dark text-white rounded" style="font-weight: bold;">로그인</button>
+                    <input class="p-2 mb-3 border" placeholder="이메일 형식의 아이디"/>
+                    <input class="p-2 mb-4 border" placeholder="비밀번호"/>
+                    <button class="p-2 w-100 text-white border rounded" style="font-weight: bold; background-color: #15a775;">로그인</button>
                 </div>        
         </div>
     </form>

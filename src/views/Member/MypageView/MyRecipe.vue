e199920cfd037fc07a41f7518203b4aa9a0eb457
shyeseon <gPtjs1222@gmail.com>
2024-06-21 12:13:38 +0900

    feat: add mypage recipe data binding #14

--- src/views/Member/MypageView/MyRecipe.vue
+++ src/views/Member/MypageView/MyRecipe.vue
@@ -1,79 +1,82 @@
 <template>
-    <div class="myRecipe">
-     <h3 class="fw-bold text-start mb-4" >나의 레시피 </h3>
-         <div class="d-flex" style="font-size:24px">
+    <div class="myRecipe" style="width:80%;  margin:auto auto;padding:50px 0 0">
+     <h4 class="fw-bold text-start mb-4" >나의 레시피 </h4>
+         <div class="d-flex" style="font-size:20px">
              <p>총 &ensp;</p>
-             <p style="color:darkseagreen"> 4</p>
+             <p style="color:darkseagreen"> {{ countR }}</p>
              <p>개</p>
          </div>
  
          <hr class="mt-0"/>
-         <div class="qcards d-flex">
-         <div class="card qnaCard mt-3" style="padding:0 ">
-             <div class="card-body text-center">
-                 <RecipeCard style="margin:0 ;padding:16px 0 0 16px; width:250px"/>
-             </div>
-         </div>
- 
-         <div class="card qnaCard mt-3" style="padding:0 ">
-             <div class="card-body text-center">
-                 <RecipeCard style="margin:0 ;padding:16px 0 0 16px; width:250px"/>
-             </div>
-         </div>
- 
-         <div class="card qnaCard mt-3" style="padding:0 ">
-             <div class="card-body text-center">
-                 <RecipeCard style="margin:0 ;padding:16px 0 0 16px; width:250px"/>
-             </div>
-         </div>
- 
-         <div class="card qnaCard mt-3" style="padding:0 ">
-             <div class="card-body text-center">
-                 <RecipeCard style="margin:0 ;padding:16px 0 0 16px; width:250px"/>
-             </div>
-         </div>
- 
- 
-         <div class="card qnaCard mt-3" style="padding:0 ">
-             <div class="card-body text-center">
-                 <RecipeCard style="margin:0 ;padding:16px 0 0 16px; width:250px"/>
-             </div>
-         </div>
- 
-     </div>
- 
-     
+         <div class="d-flex" style="flex-wrap: wrap;" >
+            <div class="qcard" v-for="(recards,index) in recipeCard" :key="index">
+                    <MypageRecipeCard :objectProp="recards"/>
+            </div>
+        </div>
      </div>
  </template>
  
  <script setup>
- import RecipeCard from '@/components/RecipeCard.vue';
+ import MypageRecipeCard from '@/components/MypageRecipeCard.vue';
+
+import { computed, ref } from 'vue';
  
+const recipeCard = ref([
+    { 
+        bno:1, 
+        btitle:"이거 맛있겠죠1", 
+        blike: 30, 
+        bhitcount:129, 
+        mname: "Sonhyeseon",
+         bdate: "2024-06-21 14:00", 
+         isActive: false 
+    },
+    { 
+        bno:2, 
+        btitle:"이거 맛있겠죠2", 
+        blike: 30, 
+        bhitcount:129, 
+        mname: "Sonhyeseon",
+         bdate: "2024-06-21 14:00", 
+         isActive: false 
+    },
+    { 
+        bno:3, 
+        btitle:"이거 맛있겠죠3", 
+        blike: 30, 
+        bhitcount:129, 
+        mname: "Sonhyeseon",
+         bdate: "2024-06-21 14:00", 
+         isActive: false 
+    },
+    { 
+        bno:4, 
+        btitle:"이거 맛있겠죠4", 
+        blike: 30, 
+        bhitcount:129, 
+        mname: "Sonhyeseon",
+         bdate: "2024-06-21 14:00", 
+         isActive: false 
+    },
+    { 
+        bno:4, 
+        btitle:"이거 맛있겠죠5", 
+        blike: 30, 
+        bhitcount:129, 
+        mname: "Sonhyeseon",
+         bdate: "2024-06-21 14:00", 
+         isActive: false 
+    },
+
+])
+const countR= computed(()=>recipeCard.value.length)
  </script>
  
  <style scoped>
- .myRecipe{
-     padding:50px 50px 0;
-     width:80%;
-     height:auto;
-     text-align:center;
-     margin:0 auto;
-     
- 
- }
- .qcards{ 
-     flex-wrap: wrap;
- }
- 
- .qnaCard{
-     border:solid 1px #e5e5e5;
-     border-radius: 0.7rem;
-     box-shadow: 0 2px 20px 0 rgba(0, 0, 3, 0.2);
-     width: 200px;
-     height:400px;
-     margin-top:3.5rem;
-     width: calc(33% - 2rem);
-     margin:0 1rem;
- }
- 
+.myRecipe{
+    width:100%;
+}
+.qcard{
+    width:calc(25%-2rem);
+}
  </style>
